090d3742c27430b5ac99b2ae8f0d3cf86fd04087
Weslley Costa <weslleycosta.ti@gmail.com>
2025-01-18 12:08:04 -0300

    CRUD loja online

--- Aula 11 e 12 - Django/loja-django/app/views.py
+++ Aula 11 e 12 - Django/loja-django/app/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Product
 
 class ProductListView (ListView):
@@ -6,3 +7,30 @@ class ProductListView (ListView):
     template_name = 'produtos_list.html'
     context_object_name = 'products'
     paginate_by = 10
+
+#Create product
+class ProductCreateView(CreateView):
+    model=Product
+    fields = ['name', 'image', 'brand', 'category', 'description','rating', 'price', 'countInStock', 'user']
+    template_name = "product_create_update.html"
+    success_url=reverse_lazy('product-list')
+
+# Read product
+class ProductDetailView(DetailView):
+    model= Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
+    
+# Update product
+class ProductUpdateView(UpdateView):
+    model=Product
+    fields = ['name', 'image', 'brand', 'category', 'description','rating', 'price', 'countInStock', 'user']
+    template_name = "product_create_update.html"
+    success_url=reverse_lazy('product-list')
+  
+  #Delete Product  
+class ProductDeleteView(DeleteView):
+    model=Product
+    template_name = "product_confirm_delete.html"
+    success_url=reverse_lazy('product-list')
+    
--- Aula 11 e 12 - Django/loja-django/core/urls.py
+++ Aula 11 e 12 - Django/loja-django/core/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from app.views import ProductListView
+from app.views import ProductListView, ProductCreateView, ProductDetailView, ProductDeleteView,ProductUpdateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ProductListView.as_view() )
+    path('', ProductListView.as_view() , name='product-list'),
+    path('create/', ProductCreateView.as_view(), name='product-create'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('<int:pk>/update/', ProductUpdateView.as_view(), name='product-update'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete')
 ]
